d905a63efe0da2db531a9384121d420434964fc6
cau1k <zero@composer.group>
2025-12-23 00:37:33 -0500

    feat: filter search

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -2,7 +2,13 @@ import { posts as postsCollection } from "fumadocs-mdx:collections/server";
 import { loader } from "fumadocs-core/source";
 import { toFumadocsSource } from "fumadocs-mdx/runtime/server";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const filteredPosts = isProd
+  ? postsCollection.filter((post) => !post.draft)
+  : postsCollection;
+
 export const posts = loader({
-  source: toFumadocsSource(postsCollection, []),
+  source: toFumadocsSource(filteredPosts, []),
   baseUrl: "/posts",
 });
